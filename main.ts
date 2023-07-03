ca0309d89cb715c95dff06f818db523d948e4238
minmin0530 <izumiyoshiki@icloud.com>
2023-07-04 01:25:27 +0900

    first commit

---
+++ main.ts
@@ -0,0 +1,111 @@
+const date1 = new Date();
+console.log( date1.getFullYear() );
+console.log( date1.getMonth() );
+console.log( date1.getDate() );
+console.log( date1.getDay() );
+console.log( date1.getHours() );
+console.log( date1.getMinutes() );
+console.log( date1.getSeconds() );
+console.log( date1.getMilliseconds() );
+console.log( );
+
+
+// const date2 = new Date(2023, 3, 10, 9, 20, 0, 0);
+// console.log( date2.getFullYear() );
+// console.log( date2.getMonth() );
+// console.log( date2.getDate() );
+// console.log( date2.getDay() );
+// console.log( date2.getHours() );
+// console.log( date2.getMinutes() );
+// console.log( date2.getSeconds() );
+// console.log( date2.getMilliseconds() );
+// console.log( );
+
+
+
+
+// const date3 = new Date(2024, 0, 1);
+// console.log( date3.getFullYear() );
+// console.log( date3.getMonth() );
+// console.log( date3.getDate() );
+// console.log( date3.getDay() );
+// console.log( date3.getHours() );
+// console.log( date3.getMinutes() );
+// console.log( date3.getSeconds() );
+// console.log( date3.getMilliseconds() );
+// console.log( );
+
+
+
+// const date4 = new Date();
+// date4.setFullYear(2020);
+// console.log( date4.getFullYear() );
+
+// date4.setMonth(0);
+// console.log( date4.getMonth() );
+
+// date4.setDate(30);
+// console.log( date4.getDate() );
+
+// date4.setHours(23);
+// console.log( date4.getHours() );
+
+// date4.setMinutes(59);
+// console.log( date4.getMinutes() );
+
+// date4.setSeconds(35);
+// console.log( date4.getSeconds() );
+
+// date4.setMilliseconds(999);
+// console.log( date4.getMilliseconds() );
+
+
+
+
+
+// const date5 = new Date();
+// date5.setFullYear(2019, 2, 3);
+// console.log( date5.toString() );
+// console.log( date5.getFullYear() );
+// console.log( );
+
+// date5.setMonth(4, 5);
+// console.log( date5.toString() );
+// console.log( date5.getMonth() );
+// console.log( );
+
+// date5.setDate(6);
+// console.log( date5.toString() );
+// console.log( date5.getDate() );
+// console.log( );
+
+// date5.setHours(21, 21, 21);
+// console.log( date5.toString() );
+// console.log( date5.getHours() );
+// console.log( );
+
+// date5.setMinutes(29, 28);
+// console.log( date5.toString() );
+// console.log( date5.getMinutes() );
+// console.log( );
+
+// date5.setSeconds(27);
+// console.log( date5.toString() );
+// console.log( date5.getSeconds() );
+// console.log( );
+
+// date5.setMilliseconds(777);
+// console.log( date5.toString() );
+// console.log( date5.getMilliseconds() );
+// console.log( );
+
+
+
+
+
+
+// const date6 = new Date();
+// console.log( date6.toString() );
+// console.log( date6.toLocaleString() );
+// console.log( date6.toJSON() );
+// console.log( date6.getTime() );
